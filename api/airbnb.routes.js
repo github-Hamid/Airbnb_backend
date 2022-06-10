442d23a28132d19da072eea13eb09ff185a5b760
github-Hamid <h.rahimi.1953@gmail.com>
2022-06-10 10:51:25 -0400

    adding routes

--- api/airbnb.routes.js
+++ api/airbnb.routes.js
@@ -7,5 +7,7 @@ router.route("/id/:id").get(AirbnbController.apiGetListById)
 router.route("/type/:type").get(AirbnbController.apiGetListByType)
 router.route("/price/:price").get(AirbnbController.apiGetListByPrice);
 router.route("/price&type").get(AirbnbController.getListByPrice_Type); 
+router.route("/price&country").get(AirbnbController.apiGetListByPrice_country);
+router.route("/type&country").get(AirbnbController.apiGetListByType_country);
 router.route("/country/:country").get(AirbnbController.apiGetListByCountry);
 export default router; 
