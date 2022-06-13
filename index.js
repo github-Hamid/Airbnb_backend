97dd107a6fbdeefab48d39bd9b6ae82798999689
github-Hamid <h.rahimi.1953@gmail.com>
2022-06-12 21:00:40 -0400

    process.env.PORT || 5000

--- index.js
+++ index.js
@@ -19,7 +19,7 @@ client.connect()
       .then((collection)=>{
         console.log("Users collection is connected successfully");
       
-        app.listen(process.env.PORT, ()=>{
+        app.listen(process.env.PORT || 5000, ()=>{
           console.log(`App is listening to port ${process.env.PORT}`); 
       })  
        
